1e8606d8bcf5d7447c5dd0e89075f2aa543b7ef7
guy <guyskv@gmail.com>
2024-05-28 21:36:56 +0300

    LifeCycle

--- app/src/main/java/com/guy/class24b_and_4/Activity_LifeCycle.java
+++ app/src/main/java/com/guy/class24b_and_4/Activity_LifeCycle.java
@@ -5,37 +5,79 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 
-public class Activity_Handler extends AppCompatActivity {
+public class Activity_LifeCycle extends AppCompatActivity {
 
 
     private MaterialTextView timer_LBL_info;
-    private MaterialButton timer_BTN_start;
-    private MaterialButton timer_BTN_stop;
 
     private int count = 0;
     private final int DELAY = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("pttt", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         timer_LBL_info = findViewById(R.id.timer_LBL_info);
-        timer_BTN_start = findViewById(R.id.timer_BTN_start);
-        timer_BTN_stop = findViewById(R.id.timer_BTN_stop);
-        timer_BTN_start.setOnClickListener(v -> start());
-        timer_BTN_stop.setOnClickListener(v -> stop());
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        Log.d("pttt", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("pttt", "onResume");
+        super.onResume();
+        start();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("pttt", "onPause");
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onStop() {
+        Log.d("pttt", "onStop");
+        super.onStop();
+        stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("pttt", "onDestroy");
+        super.onDestroy();
     }
 
     private void tick() {
         count++;
         timer_LBL_info.setText("" + count);
+        playSound();
+    }
+
+    private void playSound() {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // ------------------------------------------------------------------------------------
